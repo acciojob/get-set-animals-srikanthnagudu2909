cc167dc708089056ddb41b956bccef0c10886dab
srikanthnagudu2909 <nagudusrikanth@gmail.com>
2025-10-17 18:06:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-//complete this code
-class Animal {}
+// Animal constructor
+function Animal(species) {
+  this._species = species;
+}
 
-class Dog extends Animal {}
+// Getter for species
+Animal.prototype.species = function() {
+  return this._species;
+};
 
-class Cat extends Animal {}
+// Method to make a sound
+Animal.prototype.makeSound = function() {
+  console.log(`The ${this._species} makes a sound`);
+};
 
-// Do not change the code below this line
+// Cat constructor inheriting from Animal
+function Cat(species) {
+  Animal.call(this, species);
+}
+
+// Inherit from Animal prototype
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+// Cat-specific method
+Cat.prototype.purr = function() {
+  console.log("purr");
+};
+
+// Dog constructor inheriting from Animal
+function Dog(species) {
+  Animal.call(this, species);
+}
+
+// Inherit from Animal prototype
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+// Dog-specific method
+Dog.prototype.bark = function() {
+  console.log("woof");
+};
+
+// Do not change code below
 window.Animal = Animal;
-window.Dog = Dog;
 window.Cat = Cat;
+window.Dog = Dog;
